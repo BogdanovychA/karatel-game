6766a40b7d6c578828c8bb5944a58724998e5a89
Andrii Bogdanovych <a@bogdanovych.org>
2025-11-04 14:02:32 +0200

    Косметичні редагування

--- karatel/ui/web_gui.py
+++ karatel/ui/web_gui.py
@@ -107,32 +107,33 @@ def hero() -> None:
     st.title(TITLE)
     st.header("Персонаж")
 
-    if st.session_state.hero is None:
-        name = st.text_input("Ім'я", value="Іван")
-        profession = st.selectbox(
-            "Професія",
-            options=list(PROFESSIONS.keys()),
-            format_func=lambda x: PROFESSIONS[x].name,
-        )
-        level = st.slider("Рівень", 1, 20, 1)
-
-        if st.button("Створити героя", type="secondary"):
-            st.session_state.hero = HeroFactory.generate(
-                level=level, profession=profession, name=name
+    if 'hero' in st.session_state:
+        if not st.session_state.hero:
+            name = st.text_input("Ім'я", value="Іван")
+            profession = st.selectbox(
+                "Професія",
+                options=list(PROFESSIONS.keys()),
+                format_func=lambda x: PROFESSIONS[x].name,
             )
-            st.success(f"Героя {name} створено!")
-            st.rerun()
+            level = st.slider("Рівень", 1, 20, 1)
 
-    else:
-        st.success("Героя створено")
-        st.text(st.session_state.hero.display.show())
+            if st.button("Створити героя", type="secondary"):
+                st.session_state.hero = HeroFactory.generate(
+                    level=level, profession=profession, name=name
+                )
+                st.success(f"Героя {name} створено!")
+                st.rerun()
 
-        if st.button("Видалити героя", type="primary"):
-            st.session_state.hero = None
-            if st.session_state.game_map:
-                st.session_state.game_map = []
-            ui.clear()
-            st.rerun()
+        else:
+            st.success("Героя створено")
+            st.text(st.session_state.hero.display.show())
+
+            if st.button("Видалити героя", type="primary"):
+                st.session_state.hero = None
+                if 'game_map' in st.session_state and st.session_state.game_map:
+                    st.session_state.game_map = None
+                ui.clear()
+                st.rerun()
     back()
 
 
@@ -140,55 +141,55 @@ def enemy() -> None:
     st.title(TITLE)
     st.header("Ворог")
 
-    if st.session_state.enemy is None:
-
-        profession = st.selectbox(
-            "Професія",
-            options=list(PROFESSIONS.keys()),
-            format_func=lambda x: PROFESSIONS[x].name,
-        )
-        level = st.slider(
-            "Рівень",
-            1,
-            20,
-            1 if st.session_state.hero is None else st.session_state.hero.level,
-        )
-
-        if st.button("Створити ворога", type="secondary"):
-            st.session_state.enemy = HeroFactory.generate(
-                level=level, profession=profession, name=HeroFactory.select_name()
+    if 'enemy' in st.session_state:
+        if not st.session_state.enemy:
+            profession = st.selectbox(
+                "Професія",
+                options=list(PROFESSIONS.keys()),
+                format_func=lambda x: PROFESSIONS[x].name,
             )
-            st.success(f"Ворога {st.session_state.enemy.name} створено!")
-            st.rerun()
+            level = st.slider(
+                "Рівень",
+                1,
+                20,
+                1 if not st.session_state.hero else st.session_state.hero.level,
+            )
 
-    else:
-        st.success("Ворога створено")
-        st.text(st.session_state.enemy.display.show())
+            if st.button("Створити ворога", type="secondary"):
+                st.session_state.enemy = HeroFactory.generate(
+                    level=level, profession=profession, name=HeroFactory.select_name()
+                )
+                st.success(f"Ворога {st.session_state.enemy.name} створено!")
+                st.rerun()
 
-        if st.button("Видалити ворога", type="primary"):
-            st.session_state.enemy = None
-            ui.clear()
-            st.rerun()
+        else:
+            st.success("Ворога створено")
+            st.text(st.session_state.enemy.display.show())
+
+            if st.button("Видалити ворога", type="primary"):
+                st.session_state.enemy = None
+                ui.clear()
+                st.rerun()
     back()
 
 
 def fast() -> None:
     st.title(TITLE)
-    if st.session_state.hero is None:
+    if not st.session_state.hero:
         st.subheader("Створіть персонажа, щоб почати бійку")
 
-    if 'hero' in st.session_state and st.session_state.hero is not None:
+    if 'hero' in st.session_state and st.session_state.hero:
         with st.expander("Ваш Герой:", expanded=True):
             st.text(st.session_state.hero.display.show())
 
         if 'enemy' in st.session_state and (
-            st.session_state.enemy is None or not st.session_state.enemy.alive
+            not st.session_state.enemy or not st.session_state.enemy.alive
         ):
             st.session_state.enemy = HeroFactory.generate(
                 level=st.session_state.hero.level
             )
 
-        if 'enemy' in st.session_state and st.session_state.enemy is not None:
+        if 'enemy' in st.session_state and st.session_state.enemy:
             with st.expander("Ваш ворог:", expanded=True):
                 st.text(st.session_state.enemy.display.show())
             if st.button("Почати бій", type="primary"):
