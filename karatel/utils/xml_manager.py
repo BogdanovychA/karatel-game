671e0ff7aeefc3812519492bb250929c46c2dd0c
AndriiBogdanovych <ab@foundation101.org>
2025-11-11 21:38:47 +0200

    Оптимізував XML-завантажувач героя

--- karatel/utils/xml_manager.py
+++ karatel/utils/xml_manager.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
 
 from karatel.core.game_state_manager import gsm
@@ -17,16 +18,14 @@ def xml_hero_saver(hero: Hero, path: str) -> None:
     ET.SubElement(root, "experience").text = str(hero.experience)
     ET.SubElement(root, "lives").text = str(hero.lives)
     ET.SubElement(root, "money").text = str(hero.money)
-    # stats_root = ET.SubElement(root, 'stats')
-    # for stat_name, stat_value in hero.stats.items():
-    #     ET.SubElement(stats_root, stat_name).text = str(stat_value)
+
     skills_root = ET.SubElement(root, 'skills')
-    for index, skill in enumerate(hero.skills):
-        ET.SubElement(skills_root, "skill", index=str(index)).text = str(skill.name)
+    for skill in hero.skills:
+        ET.SubElement(skills_root, "skill").text = skill.name
 
     inventory_root = ET.SubElement(root, 'inventory')
-    for index, item in enumerate(hero.inventory):
-        ET.SubElement(inventory_root, "item", index=str(index)).text = str(item.name)
+    for item in hero.inventory:
+        ET.SubElement(inventory_root, "item").text = item.name
 
     ET.SubElement(root, "left_hand").text = hero.left_hand.name
     ET.SubElement(root, "right_hand").text = hero.right_hand.name
@@ -39,7 +38,7 @@ def xml_hero_saver(hero: Hero, path: str) -> None:
 
 def xml_hero_loader(path: str) -> Hero | None:
 
-    def create_list(parent_tag: str, child_tag: str, base: tuple) -> list:
+    def _create_list(parent_tag: str, child_tag: str, base: tuple) -> list:
 
         the_list: list = []
         parent_root = root.find(parent_tag)
@@ -50,7 +49,7 @@ def xml_hero_loader(path: str) -> Hero | None:
 
         return the_list
 
-    def find_text(text: str) -> str | None:
+    def _find_text(text: str) -> str | None:
         element = root.find(text)
         if element is not None and element.text:
             return element.text
@@ -67,26 +66,16 @@ def xml_hero_loader(path: str) -> Hero | None:
         gsm.ui.write(f"Помилка завантаження XML: {e}", log=DEBUG)
         return None
 
-    name = find_text("name")
-    profession = find_text("profession")
-    experience = int(find_text("experience") or "0")
-    lives = int(find_text("lives") or "0")
-    money = int(find_text("money") or "0")
-    left_hand = obj_finder(find_text("left_hand"), SHIELDS)
-    right_hand = obj_finder(find_text("right_hand"), WEAPONS)
-    skills = create_list('skills', 'skill', SKILLS)
-    inventory = create_list('inventory', 'item', ITEMS)
-
     hero = Hero(
-        name=name,
-        profession=obj_finder(profession, PROFESSIONS),
-        experience=experience,
+        name=_find_text("name"),
+        profession=obj_finder(_find_text("profession"), PROFESSIONS),
+        experience=int(_find_text("experience") or "0"),
     )
-    hero.lives = lives
-    hero.money = money
-    hero.skills = skills
-    hero.inventory = inventory
-    hero.right_hand = right_hand
-    hero.left_hand = left_hand
+    hero.lives = int(_find_text("lives") or "1")
+    hero.money = int(_find_text("money") or "0")
+    hero.right_hand = obj_finder(_find_text("right_hand"), WEAPONS)
+    hero.left_hand = obj_finder(_find_text("left_hand"), SHIELDS)
+    hero.inventory = _create_list('inventory', 'item', ITEMS)
+    hero.skills = _create_list('skills', 'skill', SKILLS)
 
     return hero
