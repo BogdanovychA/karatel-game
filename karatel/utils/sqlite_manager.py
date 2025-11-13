36d8f16229a6b01fd5e87b77d3b44b2ae8ea81a4
Andrii Bogdanovych <a@bogdanovych.org>
2025-11-13 20:47:25 +0200

    Видалив зайве

--- karatel/utils/sqlite_manager.py
+++ karatel/utils/sqlite_manager.py
@@ -6,69 +6,18 @@ import sqlite3
 from typing import TYPE_CHECKING
 
 from karatel.core.hero import Hero
-from karatel.utils.settings import DEBUG, SQLITE_PATH
+from karatel.utils.settings import DEBUG, LOG, SQLITE_PATH
+from karatel.utils.utils import hero_to_dict, obj_finder, sanitize_word
 
-# from karatel.core.hero import Hero
 # from karatel.core.items import ITEMS, SHIELDS, WEAPONS
 # from karatel.core.professions import PROFESSIONS
 # from karatel.core.skills import SKILLS
-# from karatel.utils.settings import DEBUG, LOG
-from karatel.utils.utils import hero_to_dict, obj_finder, sanitize_word
-
-# from karatel.utils.constants import FORBIDDEN_SQL_CHARS
 
 
 if TYPE_CHECKING:
     from karatel.ui.abstract import OutputSpace
 
-DEBUG = True
-
-FORBIDDEN_SQL_CHARS = (
-    "'",
-    '"',
-    ';',
-    '--',
-    '*',
-    '/',
-    '+',
-    '=',
-    '>',
-    '<',
-    '!',
-    '@',
-    '#',
-    '$',
-    '%',
-    '^',
-    '&',
-    '(',
-    ')',
-    '[',
-    ']',
-    '{',
-    '}',
-    '`',
-    '~',
-    '|',
-    '\\',
-    '?',
-    '.',
-    ',',
-    ' ',
-)
-
-
-def check_word(output: OutputSpace, word: str) -> bool:
-    if not word or word is True:
-        output.write(f"'{word}' не може бути пустим, None, False або True", log=DEBUG)
-        return False
-    for char in word:
-        if char in FORBIDDEN_SQL_CHARS:
-            output.write(
-                f"Неможливо виконати: '{word}' містить заборонені символи", log=DEBUG
-            )
-            return False
-    return True
+DEBUG = True  # На час розробки модуля
 
 
 def table_exists(
@@ -228,70 +177,3 @@ def select_heroes(
     except sqlite3.Error as e:
         output.write(f"Помилка SQLite: '{e}'", log=DEBUG)
         return []
-
-
-# def sqlite_hero_saver(hero: Hero, path: str, log: bool = LOG) -> None:
-#     """Збереження героя"""
-#
-#     the_dict: dict = {
-#         "name": hero.name,
-#         "profession": hero.profession.name,
-#         "experience": hero.experience,
-#         "lives": hero.lives,
-#         "money": hero.money,
-#         "left_hand": hero.left_hand.name,
-#         "right_hand": hero.right_hand.name,
-#         "skills": [],
-#         "inventory": [],
-#     }
-#     for skill in hero.skills:
-#         the_dict["skills"].append(skill.name)
-#     for item in hero.inventory:
-#         the_dict["inventory"].append(item.name)
-#
-#     try:
-#         with open(path, 'w', encoding='utf-8') as file:
-#             json.dump(the_dict, file, indent=4, ensure_ascii=False)
-#
-#         hero.output.write(f"Героя {hero.name} збережено", log=log)
-#
-#     except Exception as e:
-#         hero.output.write(f"Сталася помилка при збереженні файлу: {e}", log=log)
-#
-#
-# def sqlite_hero_loader(output: OutputSpace, path: str, log: bool = LOG) -> Hero | None:
-#     """Завантаження героя"""
-#
-#     def _create_list(input_list: list, base: tuple) -> list:
-#         """Допоміжна функція для забезпечення DRY"""
-#         the_list = []
-#         for item in input_list:
-#             the_list.append(obj_finder(item, base))
-#         return the_list
-#
-#     try:
-#         with open(path, 'r', encoding='utf-8') as file:
-#             the_dict = json.load(file)
-#
-#         hero = Hero(
-#             output=output,
-#             name=the_dict["name"],
-#             profession=obj_finder(the_dict["profession"], PROFESSIONS),
-#             experience=int(the_dict["experience"] or "0"),
-#         )
-#         hero.lives = int(the_dict["lives"] or "1")
-#         hero.money = int(the_dict["money"] or "0")
-#         hero.right_hand = obj_finder(the_dict["right_hand"], WEAPONS)
-#         hero.left_hand = obj_finder(the_dict["left_hand"], SHIELDS)
-#         hero.inventory = _create_list(the_dict["inventory"], ITEMS)
-#         hero.skills = _create_list(the_dict["skills"], SKILLS)
-#
-#         hero.output.write(f"Героя {hero.name} завантажено", log=log)
-#         return hero
-#
-#     except FileNotFoundError:
-#         output.write(f"Помилка: Файл '{path}' не знайдено.", log=DEBUG)
-#     except json.JSONDecodeError:
-#         output.write(
-#             f"Помилка: Файл '{path}' містить недійсний формат JSON.", log=DEBUG
-#         )
