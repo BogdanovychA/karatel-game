a0dee32550909b7c5db7acc358a56a8021b44760
Andrii Bogdanovych <a@bogdanovych.org>
2025-11-27 12:01:05 +0200

    Додав тест боїв

---
+++ karatel/tests/test_combats.py
@@ -0,0 +1,39 @@
+from itertools import combinations
+
+import pytest
+
+from karatel.core.hero import HeroFactory
+from karatel.core.professions import PROFESSIONS
+from karatel.logic.combat import fight
+from karatel.ui.abstract import NoneOutput
+
+output = NoneOutput()
+
+num_tests = 1000
+min_ratio = 0.45
+max_ratio = 0.55
+
+
+@pytest.mark.parametrize("level", list(range(1, 21)))
+@pytest.mark.parametrize(
+    "prof1, prof2", list(combinations(list(PROFESSIONS.keys()), 2))
+)
+def test_hero_combats(level, prof1, prof2):
+    wins1 = 0
+    wins2 = 0
+
+    for _ in range(num_tests):
+        hero1 = HeroFactory.generate(output, level=level, profession=prof1)
+        hero2 = HeroFactory.generate(output, level=level, profession=prof2)
+        fight(hero1, hero2)
+
+        if hero1.alive and not hero2.alive:
+            wins1 += 1
+        elif hero2.alive and not hero1.alive:
+            wins2 += 1
+
+    total = wins1 + wins2
+    assert total == num_tests
+
+    ratio = wins1 / total
+    assert min_ratio <= ratio <= max_ratio
